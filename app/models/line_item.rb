6c98f6b42258029cb89e2941c083322cb2ce039b
Tim Imasa <timothyimasa@gmail.com>
2019-10-11 12:26:51 -0700

    Added relationship between line items and orders

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,6 @@
 class LineItem < ApplicationRecord
+  belongs_to :order, optional: true
   belongs_to :product, optional: true
-  belongs_to :cart, optional: true
   belongs_to :cart
 
   def total_price
