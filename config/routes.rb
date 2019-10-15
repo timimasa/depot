55cd7033b599342a68f022962f075c129b26c1b7
Tim Imasa <timothyimasa@gmail.com>
2019-10-15 14:04:13 -0700

    Defined locales for en and es

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
-  controller :sessions do 
-    get 'login' => :new
+  controller :sessions do
+    get  'login' => :new
     post 'login' => :create
-    delete 'logout' => :destroy  
+    delete 'logout' => :destroy
   end
-
-  get 'admin/index'
-  get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
+
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
